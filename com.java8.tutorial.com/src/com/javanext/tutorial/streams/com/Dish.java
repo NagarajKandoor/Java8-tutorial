cd7751873fbee40aa13639f6ada2d19869ec2016
uib08758 <uib08758@OZL2988W.mshome.net>
2020-05-10 23:55:18 +0530

    Streams

---
+++ com.java8.tutorial.com/src/com/javanext/tutorial/streams/com/Dish.java
@@ -0,0 +1,53 @@
+package com.javanext.tutorial.streams.com;
+
+/**
+ * @author Nagaraj
+ *
+ */
+public class Dish {
+
+	private final String name;
+	private final boolean vegetarian;
+	private final int calories;
+	private final Type type;
+
+	public Dish(String name, boolean vegetarian, int calories, Type type) {
+		this.name = name;
+		this.vegetarian = vegetarian;
+		this.calories = calories;
+		this.type = type;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the vegetarian
+	 */
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+
+	/**
+	 * @return the calories
+	 */
+	public int getCalories() {
+		return calories;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public Type getType() {
+		return type;
+	}
+
+	public enum Type {
+		MEAT, FISH, OTHER
+	}
+
+}
